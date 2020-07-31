f5b5a294aeb9b99e50648edf05caf039adfbdce3
Achraf-haddar <achraf.haddar@supcom.tn>
2020-07-31 11:45:53 +0200

    predict.py

---
+++ src/predict.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+from sklearn import ensemble
+from sklearn import preprocessing
+from sklearn import metrics
+import joblib
+
+from . import dispatcher
+TRAINING_DATA = os.environ.get("TRAINING_DATA")
+TEST_DATA = os.environ.get("TEST_DATA")
+MODEL = os.environ.get("MODEL")
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(TEST_DATA)
+        
+    for FOLD in range(5):
+        encoders = joblib.load(os.path.join("models", f"{MODEL}_{FOLD}_label_encoder.pkl"))
+        for c in train_df.columns:
+            lbl = encoders[c]
+            df .loc[:, c] = lbl.transform(df[c].values.tolist())
+        
+        # data is ready to train
+        clf = joblib.load(os.path.join("models", f"{MODEL}_{FOLD}.pkl"))
+        cols = joblib.load(os.path.join("models", f"{MODEL}_{FOLD}_columns.pkl"))
+
+        preds = clf.predict_proba(valid_df)[:, 1]
+        
